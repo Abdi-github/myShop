5ef43a497b2f5ead71caefab7086bc462ad3616a
abdigit <abdulkadirahmed1985@gmail.com>
2020-11-14 16:05:09 +0100

    place order api created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 import dotenv from "dotenv";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
@@ -39,6 +40,7 @@ app.get("/api/products/:id", (req, res) => {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
